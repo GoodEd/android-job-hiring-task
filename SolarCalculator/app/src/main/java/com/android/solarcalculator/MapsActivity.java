933056578c841e8539b4217b9f5fc0b8591f97c3
nimesh1997 <nkumarsachan@gmail.com>
2018-12-27 17:15:28 +0530

    MoonRISE_SET

--- SolarCalculator/app/src/main/java/com/android/solarcalculator/MapsActivity.java
+++ SolarCalculator/app/src/main/java/com/android/solarcalculator/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.android.solarcalculator;
 
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
@@ -39,6 +41,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -60,6 +63,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private static final float DEFAULT_ZOOM = 15f;
 
+    Date date = new Date();
+
 
     private static final String FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = android.Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -67,6 +72,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //widgets
     private EditText msearchlocation;
+
+    public ProgressDialog progressDialog;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +96,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mr = (TextView) findViewById(R.id.mr);
         ms = (TextView) findViewById(R.id.ms);
 
+
+
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//            }
+//        });
 
         //getLocationpermission
         getLocationPermission();
@@ -111,6 +132,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+
+
         hidekeyboard();
 
         }
@@ -135,53 +158,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d(TAG, address.toString());
 
 
-            Date date = new Date();
+
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
             Toast.makeText(getApplicationContext(), dateFormat.format(date), Toast.LENGTH_SHORT).show();
 
             double lati = address.getLatitude();
             double longi = address.getLongitude();
 
+
             String dstring = dateFormat.format(date);
-            Date date1 = new Date();
-            try {
-                date1 =(Date) dateFormat.parse(dstring);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
 
-            SunTimes suntimes = SunTimes.compute()
-                    .on(date1).at(lati, longi).execute();
-            Log.d(TAG, "sunrise: " + suntimes.getRise());
-            Log.d(TAG, "sunset: " + suntimes.getSet());
+            settingtime(lati, longi, dstring); //setting the time
 
-            MoonTimes moonTimes = MoonTimes.compute()
-                    .on(date1).at(lati, longi).execute();
-            Log.d(TAG, "moonrise: " + moonTimes.getRise());
-            Log.d(TAG, "moonset: " + moonTimes.getSet());
-
-            tv1.setText(dstring);
-
-            String temp = "";
-            String srise = suntimes.getRise().toString();
-            temp = getTime(srise);
-            sr.setText(temp);
-            String sset = suntimes.getSet().toString();
-            temp = getTime(sset);
-            ss.setText(temp);
-            String mrise = moonTimes.getRise().toString();
-            temp = getTime(mrise);
-            mr.setText(temp);
-            String mset = moonTimes.getSet().toString();
-            temp = getTime(mset);
-            ms.setText(temp);
-
-
-
-
-
-
-            Log.d(TAG, "Only Time" + temp);
+//
+//
 
 
 
@@ -282,6 +272,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     Log.d(TAG, "location found");
                                     Location currLocation = (Location) task.getResult();
 
+//                                    Date date = new Date();
+                                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                                    Toast.makeText(getApplicationContext(),"default location:date " + dateFormat.format(date), Toast.LENGTH_SHORT).show();
+
+                                    String sysdate = dateFormat.format(date);
+
+//                                    String fdate =
+
+                                    settingtime(currLocation.getLatitude(),currLocation.getLongitude(), sysdate);
+
                                     moveCamera(new LatLng(currLocation.getLatitude(), currLocation.getLongitude()),DEFAULT_ZOOM, "My Location");
                                 }
                                 else {
@@ -294,6 +294,283 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }catch (SecurityException e){
             Log.e(TAG, "deviceLocation: SecurityException" + e.getMessage());
         }
+
+    }
+
+    private void settingtime(final double latitude, final double longitude, String sysdate) {
+
+//        progressDialog.show();
+//        progressDialog.setMessage("Please wait...");
+
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG, "b1 onclick: ");
+
+                final Calendar cal = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                String sys = df.format(date);
+
+                Date date1 = new Date();
+                try {
+                    date1 =(Date) df.parse(sys);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                cal.setTime(date1);
+                cal.add(Calendar.DATE, 1);
+
+                String stemp = df.format(cal.getTime());
+
+                try {
+                    date1 = (Date) df.parse(stemp);
+                }catch (ParseException e){
+                    e.printStackTrace();
+                }
+
+                date = date1;
+
+                String checkdate ="";
+
+                checkdate = dateFormat.format(date);
+
+                //string to date conversion
+                Date date2 = new Date();
+                try {
+                    date2 =(Date) dateFormat.parse(checkdate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+                SunTimes suntimes = SunTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "sunrise: " + suntimes.getRise());
+                Log.d(TAG, "sunset: " + suntimes.getSet());
+
+                MoonTimes moonTimes = MoonTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "moonrise: " + moonTimes.getRise());
+                Log.d(TAG, "moonset: " + moonTimes.getSet());
+
+//        tv1.setText(checkdate);
+
+                String temp = "";
+                String srise = suntimes.getRise().toString();
+                temp = getTime(srise);
+                sr.setText(temp);
+                String sset = suntimes.getSet().toString();
+                temp = getTime(sset);
+                ss.setText(temp);
+                if (moonTimes.getRise() != null) {
+                    String mrise = moonTimes.getRise().toString();
+                temp = getTime(mrise);
+                mr.setText(temp);}
+                if(moonTimes.getSet() != null){
+                String mset = moonTimes.getSet().toString();
+                temp = getTime(mset);
+                ms.setText(temp);}
+
+                tv1.setText(dateFormat.format(date));
+
+                date = date1;
+
+
+                Log.d(TAG, "Increment DATE: " + date);
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG, "b2 onclick: ");
+
+                final Calendar cal = Calendar.getInstance();
+                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                String sys = df.format(date);
+
+                Date date1 = new Date();
+                try {
+                    date1 =(Date) df.parse(sys);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                cal.setTime(date1);
+                cal.add(Calendar.DATE, -1);
+
+                String stemp = df.format(cal.getTime());
+
+                try {
+                    date1 = (Date) df.parse(stemp);
+                }catch (ParseException e){
+                    e.printStackTrace();
+                }
+
+                date = date1;
+
+                String checkdate ="";
+
+                checkdate = dateFormat.format(date);
+
+                //string to date conversion
+                Date date2 = new Date();
+                try {
+                    date2 =(Date) dateFormat.parse(checkdate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+                SunTimes suntimes = SunTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "sunrise: " + suntimes.getRise());
+                Log.d(TAG, "sunset: " + suntimes.getSet());
+
+                MoonTimes moonTimes = MoonTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "moonrise: " + moonTimes.getRise());
+                Log.d(TAG, "moonset: " + moonTimes.getSet());
+
+//        tv1.setText(checkdate);
+
+                String temp = "";
+                String srise = suntimes.getRise().toString();
+                temp = getTime(srise);
+                sr.setText(temp);
+                String sset = suntimes.getSet().toString();
+                temp = getTime(sset);
+                ss.setText(temp);
+                if (moonTimes.getRise() != null) {
+                    String mrise = moonTimes.getRise().toString();
+                    temp = getTime(mrise);
+                    mr.setText(temp);}
+                    if(moonTimes.getSet() != null){
+                String mset = moonTimes.getSet().toString();
+                temp = getTime(mset);
+                ms.setText(temp);}
+
+                tv1.setText(dateFormat.format(date));
+
+                date = date1;
+
+
+                Log.d(TAG, "Increment DATE: " + date);
+
+
+
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String checkdate ="";
+                date = new Date();
+                checkdate = dateFormat.format(date);
+
+                Date date2 = new Date();
+                try {
+                    date2 =(Date) dateFormat.parse(checkdate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+
+                SunTimes suntimes = SunTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "sunrise: " + suntimes.getRise());
+                Log.d(TAG, "sunset: " + suntimes.getSet());
+
+                MoonTimes moonTimes = MoonTimes.compute()
+                        .on(date2).at(latitude, longitude).execute();
+                Log.d(TAG, "moonrise: " + moonTimes.getRise());
+                Log.d(TAG, "moonset: " + moonTimes.getSet());
+
+//        tv1.setText(checkdate);
+
+                String temp = "";
+                String srise = suntimes.getRise().toString();
+                temp = getTime(srise);
+                sr.setText(temp);
+                String sset = suntimes.getSet().toString();
+                temp = getTime(sset);
+                ss.setText(temp);
+                if (moonTimes.getRise() != null) {
+                    String mrise = moonTimes.getRise().toString();
+                    temp = getTime(mrise);
+                    mr.setText(temp);}
+                if(moonTimes.getSet() != null){
+                    String mset = moonTimes.getSet().toString();
+                    temp = getTime(mset);
+                    ms.setText(temp);}
+
+                tv1.setText(dateFormat.format(date));
+
+//                date = date;
+
+
+                Log.d(TAG, "Increment DATE: " + date);
+
+            }
+        });
+
+
+        String checkd ="";
+
+        checkd = dateFormat.format(date);
+
+        //string to date conversion
+        Date date2 = new Date();
+        try {
+            date2 =(Date) dateFormat.parse(checkd);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        SunTimes suntimes = SunTimes.compute()
+                .on(date2).at(latitude, longitude).execute();
+        Log.d(TAG, "sunrise2: " + suntimes.getRise());
+        Log.d(TAG, "sunset2: " + suntimes.getSet());
+
+        MoonTimes moonTimes = MoonTimes.compute()
+                .on(date2).at(latitude, longitude).execute();
+        Log.d(TAG, "moonrise2: " + moonTimes.getRise());
+        Log.d(TAG, "moonset2: " + moonTimes.getSet());
+
+//        tv1.setText(checkdate);
+
+        String temp = "";
+        String srise = suntimes.getRise().toString();
+        temp = getTime(srise);
+        sr.setText(temp);
+        String sset = suntimes.getSet().toString();
+        temp = getTime(sset);
+        ss.setText(temp);
+        if(moonTimes.getRise() != null){
+        String mrise = moonTimes.getRise().toString();
+        temp = getTime(mrise);
+        mr.setText(temp);}
+        if(moonTimes.getSet() != null){
+        String mset = moonTimes.getSet().toString();
+        temp = getTime(mset);
+        ms.setText(temp);}
+
+
+
+//        progressDialog.dismiss();
+
+
+//        Log.d(TAG, "Only Time" + temp);
+
 
     }
 
